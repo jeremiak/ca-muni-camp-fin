8f7cf19fb53b1a3920563d1ac50cf730015a26ca
Jeremia Kimelman <jbkimelman@gmail.com>
2020-08-30 00:26:09 -0700

    Add more entities to scrape from
    Found more here https://netfile.com/connect2/api/public/campaign/agencies

--- config.js
+++ config.js
@@ -1,31 +1,179 @@
 const sitesToScrape = [
   {
+    entity: "Alameda County",
+    aid: "COA",
+  },
+  {
+    entity: "Anaheim",
+    aid: "ANA",
+  },
+  {
+    entity: "Berkeley",
+    aid: "BRK",
+  },
+  {
+    entity: "Burbank",
+    aid: "COB",
+  },
+  {
+    entity: "Butte County",
+    aid: "BCO",
+  },
+  {
+    entity: "Capitola City",
+    aid: "CAP",
+  },
+  {
+    entity: "Carlsbad",
+    aid: "CAR",
+  },
+  {
+    entity: "Carson City",
+    aid: "CRSN",
+  },
+  {
+    entity: "Chico",
+    aid: "CHCO",
+  },
+  {
+    entity: "Chula Vista",
+    aid: "CCV",
+  },
+  {
+    entity: "Contra Costa County",
+    aid: "CCC",
+  },
+  {
+    entity: "Covina",
+    aid: "CVN",
+  },
+  {
     entity: "Fresno",
     aid: "COF",
+  },
+  {
+    entity: "Gilroy",
+    aid: "GIL",
+  },
+  {
+    entity: "Glendale",
+    aid: "GLD",
+  },
+  {
+    entity: "Glendora",
+    aid: "COG",
   },
   {
     entity: "Hayward",
     aid: "HWD",
   },
   {
+    entity: "Huntington Beach",
+    aid: "CHB",
+  },
+  {
     entity: "Irvine",
     aid: "COI",
+  },
+  {
+    entity: "Kern County",
+    aid: "KERN",
+  },
+  {
+    entity: "Laguna Niguel",
+    aid: "CLN",
   },
   {
     entity: "Lake Forest",
     aid: "CLF",
   },
   {
+    entity: "Livermore",
+    aid: "LIV",
+  },
+  {
+    entity: "Madera County",
+    aid: "MAD",
+  },
+  {
+    entity: "Manteca",
+    aid: "MTA",
+  },
+  {
+    entity: "Marin County",
+    aid: "CMAR",
+  },
+  {
+    entity: "Monterey County",
+    aid: "COM",
+  },
+  {
+    entity: "Mountain View",
+    aid: "MTV",
+  },
+  {
+    entity: "Monrovia",
+    aid: "MON",
+  },
+  {
+    entity: "Murrieta",
+    aid: "CMA",
+  },
+  {
     entity: "Morgan Hill",
     aid: "MGH",
   },
   {
-    entity: "Riverside County",
-    aid: "CTRIV",
+    entity: "Nevada County",
+    aid: "NEV",
   },
   {
     entity: "Oakland",
     aid: "COAK",
+  },
+  {
+    entity: "Oceanside",
+    aid: "OCN",
+  },
+  {
+    entity: "Orange County",
+    aid: "COC",
+  },
+  {
+    entity: "Palo Alto",
+    aid: "CPA",
+  },
+  {
+    entity: "Pasadena",
+    aid: "PSDA",
+  },
+  {
+    entity: "Patterson",
+    aid: "PTSN",
+  },
+  {
+    entity: "Placer County",
+    aid: "PLA",
+  },
+  {
+    entity: "Pleasanton",
+    aid: "COP",
+  },
+  {
+    entity: "Rancho Cucamonga",
+    aid: "CRC",
+  },
+  {
+    entity: "Rialto",
+    aid: "RIAL",
+  },
+  {
+    entity: "Riverside",
+    aid: "CITRIV",
+  },
+  {
+    entity: "Riverside County",
+    aid: "CTRIV",
   },
   {
     entity: "Sacramento City",
@@ -36,6 +184,18 @@ const sitesToScrape = [
     aid: "SCO",
   },
   {
+    entity: "Salinas",
+    aid: "SLNS",
+  },
+  {
+    entity: "San Bernadino",
+    aid: "CSBN",
+  },
+  {
+    entity: "San Bernadino County",
+    aid: "SBD",
+  },
+  {
     entity: "San Diego County",
     aid: "CSD",
   },
@@ -44,8 +204,32 @@ const sitesToScrape = [
     aid: "SDM",
   },
   {
+    entity: "San Francisco",
+    aid: "SFO",
+  },
+  {
+    entity: "San Joaquin County",
+    aid: "SJC",
+  },
+  {
     entity: "San Jose",
     aid: "CSJ",
+  },
+  {
+    entity: "San Luis Obispo",
+    aid: "SLO",
+  },
+  {
+    entity: "San Luis Obispo County",
+    aid: "SLOCO",
+  },
+  {
+    entity: "San Mateo County",
+    aid: "MAT",
+  },
+  {
+    entity: "Santa Ana",
+    aid: "CSA",
   },
   {
     entity: "Santa Barbara",
@@ -56,16 +240,48 @@ const sitesToScrape = [
     aid: "CSC",
   },
   {
+    entity: "Santa Clara County",
+    aid: "SCC",
+  },
+  {
+    entity: "Santa Cruz",
+    aid: "CRUZ",
+  },
+  {
     entity: "Santa Cruz County",
     aid: "SCCO",
+  },
+  {
+    entity: "Santa Maria",
+    aid: "SMAR",
+  },
+  {
+    entity: "Santa Monica",
+    aid: "CSM",
   },
   {
     entity: "Santa Rosa",
     aid: "CSR",
   },
   {
+    entity: "Sausalito",
+    aid: "SAU",
+  },
+  {
+    entity: "Shasta County",
+    aid: "CSHA",
+  },
+  {
+    entity: "Stockton",
+    aid: "STO",
+  },
+  {
     entity: "Sunnyvale",
     aid: "COS",
+  },
+  {
+    entity: "Ventura",
+    aid: "COV",
   },
   {
     entity: "Ventura County",
@@ -74,6 +290,22 @@ const sitesToScrape = [
   {
     entity: "Victorville",
     aid: "VIC",
+  },
+  {
+    entity: "West Hollywood",
+    aid: "WEHO",
+  },
+  {
+    entity: "West Sacramento",
+    aid: "WESTSAC",
+  },
+  {
+    entity: "Westminster",
+    aid: "COW",
+  },
+  {
+    entity: "Yountville",
+    aid: "TOV",
   },
 ]
 
